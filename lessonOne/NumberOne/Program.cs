dee1f0d4f6e6184269b6c6b6412bdf36e5b73d86
DillRoom <DillRoom@yandex.ru>
2022-09-22 20:56:19 +0500

    Начало выполнения ДЗ

--- lessonOne/NumberOne/Program.cs
+++ lessonOne/NumberOne/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее:
+// a = 5;  b = 7   -> max = 7
+// a = 2;  b = 10  -> max = 10
+// a = -9; b = -3  -> max = -3
+
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int sqr = number * number;
+
+Console.WriteLine($"Результат квадрата чила {number} равен {sqr}");
