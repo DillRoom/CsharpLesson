7038febab67887c5f1028c955869ca7dd5d5c54a
DillRoom <DillRoom@yandex.ru>
2022-09-22 21:30:23 +0500

    Второе ДЗ

--- lessonOne/NumberTwo/Program.cs
+++ lessonOne/NumberTwo/Program.cs
@@ -2,3 +2,26 @@
 // 2,3,7   -> 7
 // 44,5,78 -> 78
 // 22,3,9  -> 22
+
+Console.Clear();
+
+Console.WriteLine( "Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine( "Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine( "Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    Console.WriteLine("Максимальное число = " + a);
+}
+
+else if (b > c)
+{
+    Console.WriteLine("Максимальное число = " + b);
+}
+else
+{
+    Console.WriteLine("Максимальное число = " + c);
+}
