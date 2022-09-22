d354a1328e712b9d7d555ee782bc63f6ec826e1c
DillRoom <DillRoom@yandex.ru>
2022-09-22 21:57:35 +0500

    Четвертое ДЗ

--- lessonOne/NumberFour/Program.cs
+++ lessonOne/NumberFour/Program.cs
@@ -1,4 +1,20 @@
 ﻿// Напишите программу, которая на вход примает число (N), а на выход показывает все четные числа от 1 до N:
 // 5 -> 2,4
 // 8 -> 2,4,6,8
- 
+
+Console.Clear();
+
+Console.WriteLine( "Введите число: ");
+int a2 = Convert.ToInt32(Console.ReadLine());
+a2 = Math.Abs(a2);
+int a = 1;
+
+while (a <= a2)
+{
+    if (a2 % 2 == 0)
+    {
+       Console.Write(a2); 
+    }
+    a2 = a2 -1;
+} 
+Console.WriteLine();
