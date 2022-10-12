8b6b1f8b563e318cd593df66efa7a65eb25f4b37
DillRoom <DillRoom@yandex.ru>
2022-10-12 23:42:21 +0500

    Сохранение третьего занятия

--- lessonThree/Task23/Program.cs
+++ lessonThree/Task23/Program.cs
@@ -6,5 +6,10 @@
 
 Console.Clear();
 
-Console.WriteLine("Введите трехзначное число:");
-int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i}");
+}
