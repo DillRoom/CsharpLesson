85d324a406ee6e1fff808491a38eb078db754348
DillRoom <DillRoom@yandex.ru>
2022-11-02 21:30:53 +0500

    Сохранение решения девятой задачи

--- LessionNine/Task68/Program.cs
+++ LessionNine/Task68/Program.cs
@@ -7,4 +7,40 @@
 Console.Clear();
 
 
+Console.WriteLine("Введите натуральное занчение M :");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное занчение N :");
+int M = Convert.ToInt32(Console.ReadLine());
+
+NODFunction(M,N);
+
+
+// вызов функции наибольшего общего делителя.
+void NODFunction(int m, int n)
+{
+    Console.Write(NOD(m, n)); 
+}
+
+// функция наибольшего общего делителя.
+int NOD(int m, int n)
+{
+    while (M != N)
+    {
+        if (N > M)
+        {
+        return NOD(N = N - M, M);
+        }
+        else
+        {
+        return (NOD(N, M = M - N));
+        }
+    }
+    return N; 
+}       
+
+
+
+
+
+
 
