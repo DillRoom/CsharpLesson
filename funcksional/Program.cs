ec741b6cdaf570a67d881cd0ade09dec70790fa5
DillRoom <DillRoom@yandex.ru>
2022-11-02 18:06:24 +0500

    Сохранение функций

--- funcksional/Program.cs
+++ funcksional/Program.cs
@@ -84,3 +84,15 @@ void Number ( int N,int M){
             return;
     }   
 }
+
+///Метод нахождения суммы натуральных элементов в промежутке от M до N
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
