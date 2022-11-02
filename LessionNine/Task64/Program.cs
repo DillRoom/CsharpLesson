b2ae0506d03517c0214d85562293a7b45124776e
DillRoom <DillRoom@yandex.ru>
2022-11-02 18:05:29 +0500

    Сохранение решения девятой задачи

--- LessionNine/Task64/Program.cs
+++ LessionNine/Task64/Program.cs
@@ -4,3 +4,38 @@
 
 // M = 1; N = 5 -> 2, 4
 // M = 4; N = 8 -> 4, 6, 8
+
+Console.Clear();
+
+Console.WriteLine("Введите натуральное занчение N :");
+Console.WriteLine("(Должно быть больше 1)");
+int N = int.Parse(Console.ReadLine());
+if (N < 0) {
+    Console.WriteLine($"{N} не натуральное число"); 
+    return;
+}
+Console.WriteLine("Введите натуральное занчение M :");
+Console.WriteLine("(Должно быть больше N)");
+int M = int.Parse(Console.ReadLine());
+if (M < 0) {
+    Console.WriteLine($"{M} не натуральное число");
+    return;
+}
+if (M < N) Console.WriteLine($"Введиле другие значения {M} {N}");
+
+Number(N,M);
+
+Console.WriteLine();
+
+// Нахождение чётных чисел с двумя переменными.
+
+void Number ( int N,int M){
+    N = Math.Abs(N);
+    while (N <= M){
+        if ( N % 2 == 0 || N == 0 ){
+            Console.Write($"{N}."); 
+            } 
+            Number(N+1,M);
+            return;
+    }   
+}
